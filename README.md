7f2e7abe1180c1f1e5729c6e2be2c3804cb40e5a
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2018-01-25 22:33:02 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # attachshadow
+
+[![donate](https://img.shields.io/badge/$-donate-ff69b4.svg?maxAge=2592000&style=flat)](https://github.com/WebReflection/donate)
+[![License: ISC](https://img.shields.io/badge/License-ISC-yellow.svg)](https://opensource.org/licenses/ISC)
+
 An iframe based Shadow DOM poorlyfill
+
+
+### Features
+
+  * compatible down to **IE9** with a size of about 1.3K
+  * provides an `element.attachShadow(mode)` method out of the box
+  * both mode `open` and `closed` are available. `open` is set to the `shadowRoot` property as expected.
+  * events are synthetically re-targeted to the owner, hence propagated to the parent document
+  * encapsulation is preserved, including `:host` CSS declarations
+  * each _iframe_ transparently resize accordingly to its content
+
+### What this is not
+
+  * this is not a Custom Elements or an HTMLTemplateElement polyfill. This is only the `attachShadow` method.
+  * this is not, and never will be, a 100% reliable polyfill for Shadow DOM, for the simple reason that Shadow DOM cannot be polyfilled (this is _a poorlyfill_ indeed)
+  * not yet a battle tested solution. If you have any experience with _iframe_ shenanigans please help making this poorlyfill less poor, thank you!
+
+### CDN
+
+You can use `https://unpkg.com/attachshadow` as CDN for this library.
+
+### Test
+
+Please visit the [test page](webreflection.github.io/attachshadow/test/) and click on the button to see in console the propagated event.
---
+++ index.js
@@ -0,0 +1,252 @@
+(function (attachShadow, element) {'use strict';
+  /*! (c) Andrea Giammarchi - @WebReflection (ISC) */
+  if (attachShadow in element) return;
+
+  // used to reset both iframe and its document
+  var cssReset = [
+    'display:inline-block',
+    'box-sizing:border-box',
+    'margin:0',
+    'padding:0',
+    'background:transparent',
+    'visibility:visible',
+    'width:100%',
+    'height:auto'
+  ].join(';');
+
+  // ensure the element won't have padding
+  var shadowReset = '*[data-attachshadow]{padding:0 !important}';
+
+  // ensure unique id
+  var counter = 0;
+
+  // add the shadowReset rules to the current document
+  headOf(document).insertBefore(
+    document.createElement('style'),
+    headOf(document).firstChild
+  ).textContent = shadowReset;
+
+  // patch Element.prototype with attachShadow method
+  element.attachShadow = function attachShadow(options) {
+    var iframe = this.ownerDocument.createElement('iframe');
+    // make this node reachable through shadowReset rules
+    this.setAttribute('data-attachshadow', '');
+    // also make the iframe visually less obtrusive
+    iframe.setAttribute('scrolling', 'no');
+    iframe.setAttribute('allowtransparency', 'yes');
+    iframe.setAttribute('allowfullscreen', 'yes');
+    iframe.setAttribute('frameborder', '0');
+    // reset styles just in case
+    iframe.style.cssText = cssReset;
+    // create a shadowRoot via the appended iframe
+    var shadowRoot = new ShadowContent(this.appendChild(iframe), options);
+    // and attach it if mode is open
+    if (options.mode === 'open') this.shadowRoot = shadowRoot;
+    return shadowRoot;
+  };
+
+  // returns the head or grabs it otherwise
+  function headOf(document) {
+    return document.head || document.getElementsByTagName('head')[0];
+  }
+
+  // delegate accessors such innerHTML or textContent to the iframe body
+  function indirectAccessor(context, key) {
+    return {
+      get: function () { return context()[key]; },
+      set: function (value) { context()[key] = value; }
+    };
+  }
+
+  // per each DOM change in the iframe document
+  // forces width and height of the iframe in the parent document
+  function mutationCallback(style, body, styleChecks) {
+    var width = body.offsetWidth;
+    var height = body.offsetHeight;
+    if (this.width !== width) {
+      style.width = (this.width = width) + 'px';
+    }
+    if (this.height !== height) {
+      style.height = (this.height = height) + 'px';
+    }
+    styleChecks();
+  }
+
+  // per each <style> in the document, changes its content
+  // if there is a :host selector. :host => body
+  function styleChecks() {
+    for (var i = 0, length = this.length; i < length; i++) {
+      var content = this[i].textContent;
+      if (/:host/.test(content)) {
+        this[i].textContent = content.replace(/:host/g, 'body');
+      }
+    }
+  }
+
+  // it does exactly what it says
+  function uid() {
+    return '#shadow-root:' + counter++ + '.' + (Math.random() * new Date);
+  }
+
+  // a very basic MutationObserver fallbacks
+  function SMObserver(resize) {
+    var timeout = 0;
+    // makes DOMSubtreeModified less greedy
+    function onDOMSM() {
+      clearTimeout(timeout);
+      timeout = setTimeout(resetAndResize);
+    }
+    // reset timer and resize
+    function resetAndResize() {
+      timeout = 0;
+      resize();
+    }
+    // simulates the MutationObserver API
+    // disconnet() { body.removeEventListener('DOMSubtreeModified', onDOMSM); }
+    // ain't needed
+    this.observe = function (body) {
+      body.addEventListener('DOMSubtreeModified', onDOMSM);
+    };
+  }
+
+  // the mighty ShadowContent wannabe constructor
+  function ShadowContent(iframe, options) {
+    var descriptors = {mode: {value: options.mode}};
+    var document = iframe.contentDocument;
+    var window = iframe.contentWindow;
+    var Observer = window.MutationObserver || SMObserver;
+    // in some browser there's no synchronous body when you create an iframe
+    var body = document.body || document.createElement('body');
+    var fragment = document.createDocumentFragment();
+    // the returned body might be the right one or not
+    // same goes for its document, it might be replaced
+    // once the iframe.onload is triggered (if ever)
+    // these functions pass through scoped vars to accessors
+    // ensuring the right target is always reached.
+    // the fragment is just for convenience
+    var viaBody = function () { return body; };
+    var viaDocument = function () { return document; };
+    var viaFragment = function () { return fragment; };
+
+    // attach all delegates to the shadowRoot element
+    for (var key in fragment) {
+      if (typeof fragment[key] === 'function' && key in body) {
+        descriptors[key] = {value: body[key].bind(body)};
+      } else {
+        switch (key) {
+          case 'nodeName':
+          case 'nodeType':
+          case 'tagName':
+            descriptors[key] = indirectAccessor(viaFragment, key);
+            break;
+          case 'activeElement':
+            descriptors[key] = indirectAccessor(viaDocument, key);
+            break;
+          default:
+            descriptors[key] = indirectAccessor(viaBody, key);
+            break;
+        }
+      }
+    }
+
+    // including innerHTML and textContent
+    descriptors.innerHTML = indirectAccessor(viaBody, 'innerHTML');
+    descriptors.textContent = indirectAccessor(viaBody, 'textContent');
+
+    // attaching all delegates to the returned instance
+    Object.defineProperties(this, descriptors);
+
+    // attach to the iframe some information used on onload
+    iframe.id = uid();
+    iframe.listeners = [];
+    iframe.Element = (window.Element||window.Node).prototype;
+    iframe.addEventListener = iframe.Element.addEventListener;
+
+    // if the document is not ready, set it up onload
+    if (document.readyState != "complete") {
+      iframe.onload = onload;
+      // also intercept every addEventListener happened before
+      iframe.Element.addEventListener = function () {
+        iframe.listeners.push({t: this, a: arguments});
+      };
+    }
+    // otherwise set it up right away
+    else onload();
+
+    // attached to the iframe onload and also forced as setTimeout
+    function onload() {
+      document = iframe.contentDocument;
+      var head = headOf(document);
+      // Shadow DOM simulated styles
+      head.appendChild(
+        document.createElement('style')
+      ).textContent = 'html,body{' + cssReset + '}*{margin:0}' + shadowReset;
+      // JavaScript Shadow DOM fake environment bootstrap
+      head.appendChild(document.createElement('script')).textContent = [
+        '(function(G,A,html){',
+          // IE9 needs this because parent[iframe.id]
+          // would give back the iframe window instead
+          'for(var F,i=0,l=A.length;i<l;i++){',
+            'if(A[i].id==="' + iframe.id + '"){F=A[i];break;}',
+          '}',
+          'F.Element.' + attachShadow + '=',
+            '(parent.Element||parent.Node).prototype.' + attachShadow + ';',
+          'var removeEventListener=F.Element.removeEventListener;',
+          // the idea is that any event that reaches HTML
+          // is meant to be propagated up to the parent document
+          'F.Element.addEventListener=function(t){',
+            'var self=this;',
+            'F.addEventListener.apply(self,arguments);',
+            'F.addEventListener.call(html,t,function(e){',
+              'var c=F.ownerDocument.createEvent("Event");',
+              'c.initEvent(e.type,e.bubbles,e.cancelable);',
+              'for(var k in e)c[k]=e[k];',
+              'F.parentNode.dispatchEvent(c);',
+            '});',
+          '};',
+          // and dropping inner events should stop
+          // propagating them up to the parent document ... right?
+          'F.Element.removeEventListener=function(){',
+            'removeEventListener.apply(this,arguments);',
+          '};',
+          // all listeners registered while the iframe state was incomplete
+          // should now be attached with the right method to those nodes
+          'for(var l,i=0;i<F.listeners.length;i++){',
+            'l=F.listeners[i];',
+            'F.Element.addEventListener.apply(l.t,l.a);',
+          '}',
+        '}(',
+          'window,',
+          'parent.document.getElementsByTagName("iframe"),',
+          'document.documentElement',
+        '))'
+      ].join('\n');
+      // the previously returned node most likely is full of content
+      var firstChild;
+      // which in case is different from the document one
+      if (body !== document.body) {
+        // is going to be re-appended to the live iframe body
+        while (firstChild = body.firstChild)
+          document.body.appendChild(firstChild);
+        // ensure the body reference points at the right body
+        body = document.body;
+      }
+      // every change inside the iframe should re-calculate width and height
+      var callback = mutationCallback.bind(
+        {width: 0, height: 0},
+        iframe.style,
+        body,
+        styleChecks.bind(body.getElementsByTagName('style'))
+      );
+      // the MutationObserver or its fallback is in charge of this
+      new Observer(callback).observe(body, {
+        childList: true,
+        attributes: true,
+        subtree: true
+      });
+      // but regardless, first ready document should calculate its size
+      callback();
+    }
+  }
+
+}('attachShadow', (window.Element || window.Node).prototype));
